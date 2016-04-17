538a6dcf6d1865df4750395a1f55b262cad955eb
Nguyen, Peter HT <ptn24@mit.edu>
2016-04-17 00:55:17 -0700

    Implement K-means clustering and PCA, and use them to compress images.
    We implement the K-means clustering algo, including the cluster assignment
    stage, the centroid update stage, and the random initialization stage.
    
    After implementing K-means clustering, we apply it to compress images.
    
    We additionally implement PCA. In the first stage, we compute the
    covariance matrix of the data. In the second stage, we use singular value
    decomposition to compute the eigenvectors (principal components) of the
    data.
    
    Finally, we apply PCA to compress (via PCA projection) and reconstruct
    (via PCA reconstruction) images.

---
+++ problemSets/week8/machine-learning-ex7/src/m/plotProgresskMeans.m
@@ -0,0 +1,36 @@
+function plotProgresskMeans(X, centroids, previous, idx, K, i)
+%PLOTPROGRESSKMEANS is a helper function that displays the progress of 
+%k-Means as it is running. It is intended for use only with 2D data.
+%   PLOTPROGRESSKMEANS(X, centroids, previous, idx, K, i) plots the data
+%   points with colors assigned to each centroid. With the previous
+%   centroids, it also plots a line between the previous locations and
+%   current locations of the centroids.
+%
+
+% Plot the examples
+plotDataPoints(X, idx, K);
+
+% Plot the centroids as black x's
+if i == 1
+    % The initial centroids will be plotted as blue circles.
+    plot(centroids(:,1), centroids(:,2), 'o', ...
+         'MarkerEdgeColor', 'b', ...
+         'MarkerSize', 8, 'LineWidth', 2);
+else
+    % The centroids in all successive iterations of K-means will be plotted
+    % as black x's.
+    plot(centroids(:,1), centroids(:,2), 'x', ...
+         'MarkerEdgeColor', 'k', ...
+         'MarkerSize', 10, 'LineWidth', 3);
+end
+
+% Plot the history of the centroids with lines
+for j=1:size(centroids,1)
+    drawLine(centroids(j, :), previous(j, :));
+end
+
+% Title
+title(sprintf('Iteration number %d', i))
+
+end
+
